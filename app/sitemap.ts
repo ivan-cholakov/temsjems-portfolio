fd062f0d669bb11b43f6b09f3632180260af2d0f
Aventus <icholakov1@gmail.com>
2026-05-05 20:13:06 +0300

    fix: settings and deploy

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { PROJECTS, SITE } from "@/content/site";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date();
   const base = SITE.url.replace(/\/$/, "");
